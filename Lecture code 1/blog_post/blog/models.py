8238c81766543f4294c168954239361895a2e54b
MariamKipshidze <mari.kifshidze@gmail.com>
2025-10-22 20:36:08 +0400

    BP-3566 - banner_image property to blog post model

--- Lecture code 1/blog_post/blog/models.py
+++ Lecture code 1/blog_post/blog/models.py
@@ -59,9 +59,9 @@ class BlogPost(models.Model):
     def get_images(self):
         return BlogPostImage.objects.filter(blog_post=self.id)
 
-    # @property
-    # def banner_image(self):
-    #     return self.banner_image if self.banner_image else None
+    @property
+    def banner_image(self):
+        return self.banner_image if self.banner_image else None
 
     class Meta:
         verbose_name = "Blog Post"
