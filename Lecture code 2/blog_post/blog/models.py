92eef4903d2615ef5e6529511c9fb621733f3146
MariamKipshidze <mari.kifshidze@gmail.com>
2025-10-23 20:19:12 +0400

    ylbxN7PG - Add new field age to author model

--- Lecture code 2/blog_post/blog/models.py
+++ Lecture code 2/blog_post/blog/models.py
@@ -10,6 +10,7 @@ class Author(models.Model):
     email = models.EmailField(verbose_name='Email')
     birth_date = models.DateField(verbose_name='Birth date', null=True)
     profile_image = models.ImageField(verbose_name='Profile image', null=True)
+    age = models.IntegerField(verbose_name='Age', null=True)
 
     @property
     def age(self) -> int:
